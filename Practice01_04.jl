1e24666e5ede3d852bf4863dff2a18cf57668683
Kamilaisk <72505703+Kamilaisk@users.noreply.github.com>
2021-05-09 12:48:04 +0300

    Update Practice01_04.jl

--- Practice01_04.jl
+++ Practice01_04.jl
@@ -1,2 +1,32 @@
-На основе разработанных в пункте 1 функций, сотрирующих одномерный массив, написать соответствующие функции, которые бы могли получать на вход матрицу, 
-#и сортировать каждый из ее столбцов по отдельности. Имена функций оставить прежними, что были и в пункте 1, воспользовавшись механизмом множественной диспетчеризации языка Julia.
+#На основе разработанных в пункте 1 функций, сотрирующих одномерный массив, написать соответствующие функции, которые бы могли получать на вход матрицу, 
+#и сортировать каждый из ее столбцов по отдельности
+
+#Для матрицы
+function bubbleSort!(A::Matrix)
+    _ , column_size = size(A)
+    for i in 1:column_size
+        column = @view A[:, i]
+        bubbleSort!(column)
+    end
+end
+
+bubbleSort(A::Matrix) = bubbleSort!(copy(A))
+
+function BubbleSortPerm!(A::Matrix)
+    row_size , column_size = size(A)
+    B = copy(A)
+    k = 0
+    for i in 1:column_size
+        column = @view A[:, i]
+        index_array = BubbleSortPerm!(column)
+        index_column = @view B[: , i]
+        for j in 1:row_size
+            index_column[j] = index_array[j] + k
+        end
+        k += 2
+    end
+
+    return B
+end
+
+BubbleSortPerm(A::Matrix) = BubbleSortPerm!(copy(A))
