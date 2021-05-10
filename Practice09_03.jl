a6c2811cd957e143866104119ff2a0d3522ab6ab
Kamilaisk <72505703+Kamilaisk@users.noreply.github.com>
2021-05-11 01:28:00 +0300

    Update Practice09_03.jl

--- Practice09_03.jl
+++ Practice09_03.jl
@@ -1,27 +1,10 @@
-function sqrt(x,ε)
-    an=1
-    sum=1
-    i=1
-    a=0
-    while(abs(a)>ε)
-        an*=((-1)*(2*i-1)/(2*i))*x
-        a=(((-1)^(i%2))*x_minus_1)/(i+1)
-        sum+=a
-        i+=1
-    end
-    return sum
-end
-
-function ln(x,ε)
-    x_minus_1=x-1
-    sum=x_minus_1
-    i=1
-    a=0
-    while(abs(a)>ε)
-        x_minus*=x_minus
-        a=(((-1)^(i%2))*x_minus_1)/(i+1)
-        sum+=a
-        i+=1
-    end
-    return sum
+function build_cos_plots()
+    f1(x) = 1-(x^2)/2
+    f2(x) = f1(x) + (x^4)/factorial(4)
+    f3(x) = f2(x) - (x^6)/factorial(6) + (x^8)/factorial(8)
+    f4(x) = f3(x) - (x^10)/factorial(10) + (x^12)/factorial(12) - (x^14)/factorial(14) + (x^16)/factorial(16)
+    p=plot(f1) # for i in (f1,f2,f3,f4) не работает
+    plot!(f2)
+    plot!(f3)
+    plot!(f4)
 end
