01295110ebdb3089be7aba655ef6a676a12747dc
Kamilaisk <72505703+Kamilaisk@users.noreply.github.com>
2021-05-11 00:01:27 +0300

    Update and rename Practice02_01.jl to Practice03_01.jl

---
+++ Practice03_01.jl
@@ -0,0 +1,10 @@
+#транспонирование матрицы с использованием массива
+function transpose_with_array!(A::Matrix{T})::Matrix{T} where T
+    B=Matrix{T}(undef,size(A,2),size(A,1))
+    for i in 1:size(A,1)
+        for j in 1:size(A,2)
+            B[j,i]=A[i,j]
+        end
+    end
+    return B
+end
