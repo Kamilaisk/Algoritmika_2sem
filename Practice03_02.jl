0435fd745438386019d4e45b5c82ac787d492a75
Kamilaisk <72505703+Kamilaisk@users.noreply.github.com>
2021-05-11 00:03:10 +0300

    Create Practice03_02.jl
    create

---
+++ Practice03_02.jl
@@ -0,0 +1,11 @@
+#перестановка элементов в массиве без использования дополнительного массива
+function transpose!(A::Matrix{T})::Matrix{T} where T
+    for i in 1:size(A,1)
+        for j in 1:i
+            tmp = a[i,j]
+            a[i,j] = a[j,i]
+            a[j,i] = tmp
+        end
+    end
+    return A
+end
