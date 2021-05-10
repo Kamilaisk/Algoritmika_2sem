59f236bc63dba0ce4029c055b8bcff94c1004a39
Kamilaisk <72505703+Kamilaisk@users.noreply.github.com>
2021-05-10 23:07:07 +0300

    Create Practice01_08.jl

---
+++ Practice01_08.jl
@@ -0,0 +1,8 @@
+#функциz insertsort! с помощью встроенной функции reduce
+insertsort!(A)=reduce(1:length(A))do _, k # в данном случае при выполнении операции вставки  первый аргумент фуктически не используется
+    while k>1 && A[k-1] > A[k]
+        A[k-1], A[k] = A[k], A[k-1]
+        k-=1
+    end
+    return A
+end
