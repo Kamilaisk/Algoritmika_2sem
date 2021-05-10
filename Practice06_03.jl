c05aed152a3289100555d87076f04950cd06b7ae
Kamilaisk <72505703+Kamilaisk@users.noreply.github.com>
2021-05-11 01:01:22 +0300

    Create Practice06_03.jl

---
+++ Practice06_03.jl
@@ -0,0 +1,13 @@
+#функция, выполняющую частичную сортировку
+function sort_2(A::Vector{T},b::Int64)::Vector{T} where T
+    A1 = Vector{Int}(undef,0)
+    A2 = Vector{Int}(undef,0)
+    for a in A
+        if (a<=b)
+            push!(A1,a)
+        else
+            push!(A2,a)
+        end
+    end
+    return append!(A1,A2)
+end
