37861c492bad87159921ce81d23187bf1e2bf281
Kamilaisk <72505703+Kamilaisk@users.noreply.github.com>
2021-05-11 09:46:07 +0300

    Rename

--- Practice03_01.jl
+++ Practice03_01.jl
@@ -1,4 +1,4 @@
-#транспонирование матрицы с использованием массива
+#транспонирование матрицы с использованием массива 
 function transpose_with_array!(A::Matrix{T})::Matrix{T} where T
     B=Matrix{T}(undef,size(A,2),size(A,1))
     for i in 1:size(A,1)
