ad10d8ba468569eb18ae9e58a14d1b1f4840f4b2
Kamilaisk <72505703+Kamilaisk@users.noreply.github.com>
2021-06-01 11:14:19 +0300

    Create Practice13_05.jl

---
+++ Practice13_05.jl
@@ -0,0 +1,25 @@
+#определить тип, позволяющий итерировать все разбиения заданного натурального числа n на положительные слагаемые
+struct NumSplit{N} <: AbstractCombinObject
+    value::Vector{Int}
+    num_terms::Int
+end
+
+NumSplit{N}() where N = NumSplit{N}(collect(1:N), N)
+
+function next!(split::NumSplit) 
+    if split.num_terms == 1
+        false
+    end
+    s=split.value
+    k=split.num_terms
+    i=k-1
+    while i > 1 && s[i-1]==s[i]
+        i -= 1
+    end
+    s[i] += 1
+    r=sum(@view s[i+1:k])
+    k=i+r-1
+    s[i+1:k] .= 1
+    split.num_terms = k
+    return true
+end
