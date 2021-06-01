dae802d912a8e011157fa41a44e8cc2599beb9d2
Kamilaisk <72505703+Kamilaisk@users.noreply.github.com>
2021-06-01 11:05:16 +0300

    Create Practice13_01.jl

---
+++ Practice13_01.jl
@@ -0,0 +1,23 @@
+#определить тип, позволяющий итерировать все размещения с повторениями из n элементов по k
+
+struct RepPlacement{N,K} <: AbstractCombinObject
+    value::Vector{Int}
+    set::Vector
+end
+
+RepPlacement{K}(n::Integer) where K = RepPlacement{K}(ones(Int, K),collect(1:n))
+RepPlacement{K}(set::Set) where K = RepPlacement{K}(ones(Int, K),collect(set))
+
+Base.get(placement::RepPlacement) = placement.set(placement.value)
+
+function next!(placement::RepPlacement)
+    c = placement.value
+    n = length(placement.set)
+    i = findlast(item->item < n, c)
+    if isnothing(i)
+        return false
+    end
+    c[i] += 1
+    c[i+1:end] .= 1
+    return true
+end
