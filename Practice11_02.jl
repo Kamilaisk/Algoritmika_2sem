4614de445d0198e3b5562cf5d6e22383a75c600a
Kamilaisk <72505703+Kamilaisk@users.noreply.github.com>
2021-06-01 10:01:51 +0300

    Create Practice11_02.jl

---
+++ Practice11_02.jl
@@ -0,0 +1,5 @@
+#функциz, которая получает на вход массив точек плоскости типа Point2D и отображает их на графике
+Plots.scatter 
+#пример
+#points = randpoints(rand, 20)
+#scatter(points; legend = false) 
