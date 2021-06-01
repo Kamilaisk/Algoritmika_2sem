aaf0ea40582653f372cd6989d4675b8b335a0311
Kamilaisk <72505703+Kamilaisk@users.noreply.github.com>
2021-06-01 10:03:31 +0300

    Create Practice11_03.jl

---
+++ Practice11_03.jl
@@ -0,0 +1,12 @@
+#функциz, получающая вектор кортежей, 
+#содержащих пары точек типа Vector2D, и возвращающая графический объект (типа Plots.Plot), содержащий изображение соответствующих отрезков, расположенных на плоскости
+function plotsegments(segments::Vector{Tuple{Point2D,Point2D}}; kwargs...)
+    p=plot(;kwargs...)
+    for s in segments
+        plot!(collect(s); kwargs...)
+    end
+    return p
+end
+
+#пример использования
+segments = [s for s in zip(randpoints(randn,20), randpoints(randn,20))]
