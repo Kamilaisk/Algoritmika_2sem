8feb3ec84fbdfbac189472dce3d55b973bb9a55e
Kamilaisk <72505703+Kamilaisk@users.noreply.github.com>
2021-06-28 16:55:24 +0300

    Create Practice16_02.jl

---
+++ Practice16_02.jl
@@ -0,0 +1,11 @@
+#алгоритм Форда-Беллмана
+function ford_bellman(G::AbstractMatrix, s::Integer)
+    n = size(G,1)
+    C = G[s,:]
+    for k in 0:n-2, j in 1:n, i in 1:n
+        if C[j] > C[i] + G[i,j]
+            C[j] = C[i] + G[i,j]
+        end
+    end
+    return C
+end
