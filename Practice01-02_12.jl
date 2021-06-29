3301d186125c32118fdd83a9cb1d7479f75c8548
Kamilaisk <72505703+Kamilaisk@users.noreply.github.com>
2021-06-29 08:15:15 +0300

    Update Practice01-02_12.jl

--- Practice01-02_12.jl
+++ Practice01-02_12.jl
@@ -1,7 +1,7 @@
 #функция, возвращающая вектор, составленный из индексов элементов входной последовательности, на которых заданная функция достигает максимального значения 
 function findallmax(f::Function, a)
-    a1 = copy(a)
-    for i in 1:length(a1)
-        a1[i] = f(a1[i])
-    return findallmax(a1)
+    b = copy(a)
+    for i in 1:length(b)
+        b[i] = f(b[i])
+    return findallmax(b)
 end
